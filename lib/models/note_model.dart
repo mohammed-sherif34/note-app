398596a737945d9d48e14032843341dc77e44599
mohammed-sherif34 <mohammedshereef342002@gmail.com>
2023-09-25 15:36:11 +0300

    create tybe adabter for note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,20 @@
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subbtitle;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.subbtitle,
+      required this.date,
+      required this.color});
+}
